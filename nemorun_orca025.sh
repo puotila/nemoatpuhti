f73ad82161d832ae0ed37b626450ab5aa11e613a
Petteri Uotila <petteri.uotila@helsinki.fi>
2021-09-10 15:43:06 +0300

    runscripts for orca2 and orca025

--- nemorun_orca025.sh
+++ nemorun_orca025.sh
@@ -3,16 +3,16 @@
 ### parallel job script example
 ###
 ## name of your job
-#SBATCH --job-name=o2
+#SBATCH --job-name=o025
 #SBATCH --account=project_2000789
-#SBATCH --time=00:30:00
+#SBATCH --time=02:00:00
 #SBATCH --mem-per-cpu=2G
-#SBATCH --ntasks=16
-#SBATCH --partition=small
+#SBATCH --ntasks=128
+#SBATCH --partition=large
 
 module purge
 module load StdEnv intel/19.0.4 intel-mpi/18.0.5
 module load hdf5/1.10.4-mpi netcdf-fortran/4.4.4 netcdf/4.7.0 
 ## run my MPI executable
-srun ./nemo
+srun ./nemo.exe
 
