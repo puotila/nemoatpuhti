60fbd049d88a6d55871c3375c7a3cfc0223a3116
Petteri Uotila <petteri.uotila@helsinki.fi>
2021-09-18 21:57:51 +0300

    a bug fix and more memory

--- nemorun_orca025.sh
+++ nemorun_orca025.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=o025
 #SBATCH --account=project_2000789
 #SBATCH --time=02:00:00
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --ntasks=128
 #SBATCH --partition=large
 
@@ -14,5 +14,5 @@ module purge
 module load StdEnv intel/19.0.4 intel-mpi/18.0.5
 module load hdf5/1.10.4-mpi netcdf-fortran/4.4.4 netcdf/4.7.0 
 ## run my MPI executable
-srun ./nemo.exe
+srun ./nemo
 
